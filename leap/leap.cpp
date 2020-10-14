c36ff661fb2dd39ec0acdce1219a21332f198faf
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-10-14 02:38:53 +0200

    Solve exercise "Leap"

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -1,5 +1,17 @@
 #include "leap.h"
 
+namespace stdp {
+
+  inline bool divbl(const int a, const int b) {
+    return a % b == 0;
+  }
+
+}
+
 namespace leap {
+
+  bool is_leap_year(const int y) {
+    return stdp::divbl(y, 4) and (not stdp::divbl(y, 100) or stdp::divbl(y, 400));
+  }
 
 }  // namespace leap
--- leap/leap.h
+++ leap/leap.h
@@ -1,8 +1,15 @@
-#if !defined(LEAP_H)
-#define LEAP_H
+#pragma once
+
+#define EXERCISM_RUN_ALL_TESTS
 
 namespace leap {
 
+  bool is_leap_year(const int);
+
 }  // namespace leap
 
-#endif // LEAP_H
+namespace stdp {  // "std prime"
+
+  inline bool divbl(const int, const int);
+
+}  // namespace leap
