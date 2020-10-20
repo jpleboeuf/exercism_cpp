1324252e908b8d892d46801c97b33d41ffc90fe1
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-10-21 01:29:22 +0200

    Solve exercise "Gigasecond"

--- gigasecond/gigasecond.cpp
+++ gigasecond/gigasecond.cpp
@@ -1,5 +1,17 @@
 #include "gigasecond.h"
 
+using namespace boost::posix_time;
+
 namespace gigasecond {
+
+  constexpr unsigned long long int operator"" _G(unsigned long long int ulli) {
+    return ulli * 1000 * 1000 * 1000;
+  }
+
+  const time_duration td_gigasecond = seconds(1_G);
+
+  const ptime advance(const ptime & t) {
+    return t + td_gigasecond;
+  }
 
 }  // namespace gigasecond
--- gigasecond/gigasecond.h
+++ gigasecond/gigasecond.h
@@ -1,8 +1,11 @@
-#if !defined(GIGASECOND_H)
-#define GIGASECOND_H
+#pragma once
+
+#define EXERCISM_RUN_ALL_TESTS
+
+#include "boost/date_time/posix_time/posix_time.hpp"
 
 namespace gigasecond {
 
+  const boost::posix_time::ptime advance(const boost::posix_time::ptime &);
+  
 }  // namespace gigasecond
-
-#endif // GIGASECOND_H
